99fca5eb6f5e4f03db1677074406977f1625bd9e
kpdecker <kpdecker@gmail.com>
2011-02-03 16:06:57 -0600

    initState test

--- test/user-image-cache.js
+++ test/user-image-cache.js
@@ -5,4 +5,18 @@
 $(document).ready(function(){
     module("UserImageCache");
 
+    test("initState", function() {
+        expect(5);
+        UserImageCache.reset();
+        equals(UserImageCache.getEntryId(), undefined, "getEntryName");
+        equals(UserImageCache.getDisplayName(), undefined, "getDisplayName");
+        equals(UserImageCache.getSrc(), undefined, "getSrc");
+
+        raises(function() {
+            UserImageCache.load(ONE_PX_IMAGE);
+        }, "Must throw if setImageEl has not been defined")
+
+        UserImageCache.setImageEl(document.getElementById("load-image"));
+        ok(true, "setImageEl Success");
+    });
 });
